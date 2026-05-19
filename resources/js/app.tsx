c6e86e17c4c0a50e5f365b58668829df8eaa93db
Taslimul Islam <taslimul4001@gmail.com>
2026-05-19 23:06:27 +0600

    turn off appearance based theme

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,7 +1,7 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { Toaster } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { initializeTheme } from '@/hooks/use-appearance';
+// import { initializeTheme } from '@/hooks/use-appearance';
 import AppLayout from '@/layouts/app-layout';
 import AuthLayout from '@/layouts/auth-layout';
 import SettingsLayout from '@/layouts/settings/layout';
@@ -37,4 +37,4 @@ createInertiaApp({
 });
 
 // This will set light / dark mode on load...
-initializeTheme();
+// initializeTheme();
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
-        <script>
+        <!-- <script>
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
 
@@ -17,7 +17,7 @@
                     }
                 }
             })();
-        </script>
+        </script> -->
 
         {{-- Inline style to set the HTML background color based on our theme in app.css --}}
         <style>
